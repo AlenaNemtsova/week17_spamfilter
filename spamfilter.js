7b22fa823ce5f7b9f8aeaceb0b7aa77e64ff2368
Alena Nemtsova <jelenatbs@gmail.com>
2022-09-19 11:10:34 +0300

    added condition for displaying a comment

---
+++ spamfilter.js
@@ -0,0 +1,35 @@
+const inputText = document.querySelector('.input');
+const button = document.querySelector('.send-btn');
+
+const comments = [];
+
+function handler() {
+
+    if (inputText.value !== '') {
+
+        function checkSpam() {
+            return inputText.value.replace(/viagra|xxx/gi, '***');
+        }
+
+        const checkedText = checkSpam();
+
+        comments.push(checkedText);
+        console.log(comments);
+
+        let newComment = '';
+
+        for (let item of comments) {
+            newComment += `<p class="new">${item}</p>`;
+            document.querySelector('.output').innerHTML = newComment;
+        }
+
+        inputText.value = '';
+    }
+}
+
+button.addEventListener('click', handler);
+
+
+
+
+
